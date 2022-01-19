b3a63f15d69bdc7409a76d28cbb4a325982e3f09
RealPenguin <mrjanggo858@gmail.com>
2022-01-19 13:48:29 +0700

    update

---
+++ pages/category/[name].js
@@ -0,0 +1,21 @@
+import Container from '@/components/Container'
+import defaultMeta from '@/lib/defaultMeta'
+import Head from 'next/head'
+
+export default function Category() {
+    return (
+        <div>
+            <Head>
+                <title>サイト・現場猫ブログ</title>
+                <meta name="description" content={defaultMeta.meta.description} />
+                <meta name='image' content={defaultMeta.meta.image} />
+                <meta property="og:title" content={defaultMeta.meta.title} />
+                <meta property="og:description" content={defaultMeta.meta.description} />
+                <meta property="og:image" content={defaultMeta.meta.image} />
+            </Head>
+            <Container>
+                
+            </Container>
+        </div>
+    )
+}
